5cced3661cb7ba047d6b1038e3a695027639f17a
anasiqbal <anasiqbal0009@gmail.com>
2023-04-17 00:31:52 +0500

    Updated

--- backend/app.js
+++ backend/app.js
@@ -6,14 +6,10 @@ const path=require("path")
 const app=express()
 const port=process.env.PORT || 4000  
 
-app.use(express.static(path.join(__dirname,'build')));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 app.use('/',require('./routes/router.js'))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'build','index.html'));
-  });
 
 app.listen(port,()=>{
     console.log(`server is Running at https://localhost:${port}`)
