bbc4f7eb0344cc172caee6bb6730235d4b8a1ff4
anasiqbal <anasiqbal0009@gmail.com>
2023-04-17 22:51:02 +0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -8,27 +8,10 @@ const http = require('http');
 const app=express()
  
 
-app.use(express.static(path.join(__dirname,'build')));
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(cors())
 app.use('/',require('./routes/router.js'))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname,'build', 'index.html'));
-  });
-  app.use(cors({
-    origin: ''
-}));
-const whitelist = ['http://localhost:4000', 'http://13.50.249.180:4000']
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error())
-    }
-  }
-}
  
   const PORT = 4000;
   
